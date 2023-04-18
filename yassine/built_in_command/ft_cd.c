09bee942ffef93bc37d8c1a34e58fa316aa99382
yassine542 <101680065+yassine542@users.noreply.github.com>
2023-04-18 18:04:58 +0000

    fix ft_cd function

--- yassine/built_in_command/ft_cd.c
+++ yassine/built_in_command/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: yajallal <yajallal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 22:18:08 by yajallal          #+#    #+#             */
-/*   Updated: 2023/04/06 00:28:46 by yajallal         ###   ########.fr       */
+/*   Updated: 2023/04/18 18:00:42 by yajallal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,21 @@
 
 int ft_cd(char *path)
 {
-	char **splitpath;
-	char *current_path;
 	char *new_path;
 	char **home;
 	int i;
 	
 	i = 0;
-	current_path = ft_pwd();
-	splitpath = ft_split(current_path, '/');
-
-	if (path[0] == '/')
-		new_path = path;
-	else if (path[0] == '~')
+	if (path[0] == '~')
 	{
 		home = ft_split(path, '/');
+		free(home[0]);
 		home[0] = getenv("HOME");
 		new_path = multiple_join(home, ft_strlen2d(home));
+		free(path);
+		path = new_path;
 	}
-	else if (ft_strlen(path) == 1 && path[0] == '.')
-		new_path = current_path;
-	else if (ft_strlen(path) == 2 && path[0] == '.' && path[1] == '.')
-		new_path = multiple_join(splitpath, ft_strlen2d(splitpath) - 1);
-	else if (!ft_strchr(path, '/'))
-	{
-		path = ft_strjoin("/", path);
-		new_path = ft_strjoin(current_path, path);
-	}
-	if (chdir(new_path) != 0)
+	if (chdir(path) != 0)
 	{
 		printf("minishell: cd: %s: No such file or directory\n",path);
 		return (0);
@@ -49,18 +36,19 @@ int ft_cd(char *path)
 	return (1);
 }
 
-// int main()
-// {
-// 	char **line = ft_split(ft_pwd(), '/');
-// 	char *prompt = ft_strjoin(line[ft_strlen2d(line) - 1], " -> ");
-// 	char *input;
-// 	// ft_cd("~/Desktop");
-// 	// printf("%s\n", ft_pwd());
-// 	while(1)
-// 	{
-// 		input = readline(prompt);
-// 		if (ft_cd(input))
-// 			prompt = ft_strjoin(input, " -> ");
-// 		printf("%s\n", ft_pwd());
-// 	}
-// }
+int main()
+{
+	char **line = ft_split(ft_pwd(), '/');
+	char *prompt = ft_strjoin(line[ft_strlen2d(line) - 1], " -> ");
+	char *input;
+	// ft_cd("../../");
+	// // printf("%s\n", ft_pwd());
+	while(1)
+	{
+		input = readline(prompt);
+		ft_cd(input);
+		printf("%s\n", ft_pwd());
+	}
+	// chdir("../../../../Desktop");
+	// printf("%s\n",ft_pwd());
+}
