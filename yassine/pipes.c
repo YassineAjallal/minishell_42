6dfd988251235f883424c709138312add201ed1f
yassine542 <101680065+yassine542@users.noreply.github.com>
2023-04-30 17:50:00 +0100

    finish the multipiping function

--- yassine/pipes.c
+++ yassine/pipes.c
@@ -6,23 +6,16 @@
 /*   By: yajallal <yajallal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 13:57:21 by yajallal          #+#    #+#             */
-/*   Updated: 2023/04/30 15:27:51 by yajallal         ###   ########.fr       */
+/*   Updated: 2023/04/30 17:49:08 by yajallal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int init_pipe(int **pipes, int nb_pipe)
+int init_pipe(int pipes[][2], int nb_pipe)
 {
 	int i;
 	
-	i = 0;
-	pipes = malloc(sizeof(int *) * nb_pipe);
-	while (i < nb_pipe)
-	{
-		pipes[i] = malloc(sizeof(int) * 2);
-		i++;
-	}
 	i = 0;
 	while (i < nb_pipe)
 	{
@@ -33,7 +26,7 @@ int init_pipe(int **pipes, int nb_pipe)
 	return (1);
 }
 
-void close_pipes(int **pipes, int nb_pipe)
+void close_pipes(int pipes[1][2], int nb_pipe)
 {
 	int i;
 	
@@ -46,59 +39,51 @@ void close_pipes(int **pipes, int nb_pipe)
 	}
 }
 
-int pipes(t_command **cmds, t_global_info *g_info)
+int pipes(t_command **cmds, int nb_pipe)
 {
 	pid_t	pid;
-	int		**pipe_arr;
+	int		pipe_arr[nb_pipe][2];
 	int		i;
+	int		dup_stdin;
+	int		dup_stdout;
 
-	i = 0;
-	if (!init_pipe(pipe_arr, g_info->nb_pipe))
+	i = -1;
+	if (!init_pipe(pipe_arr, nb_pipe))
 		return (0);
-	pid = fork();
-	if (pid == 0)
+	dup_stdin = 0;
+	dup_stdout = pipe_arr[i][1];
+	while(++i < nb_pipe + 1)
 	{
-		dup2(pipe_arr[i][1], STDOUT_FILENO);
-		close_pipes(pipe_arr, g_info->nb_pipe);
-		cmd_exec(cmds[i]);
+		if (i != 0)
+			dup_stdin = pipe_arr[i - 1][0];
+		if (i == nb_pipe)
+			dup_stdout = 1;
+		else
+			dup_stdout = pipe_arr[i][1];
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(dup_stdin, STDIN_FILENO);
+			dup2(dup_stdout, STDOUT_FILENO);
+			close_pipes(pipe_arr, nb_pipe);
+			cmd_exec(cmds[i]);
+		}
 	}
-	i++;
-	// while(i < g_info->nb_pipe)
-	// {
-	// 	pid = fork();
-	// 	if (pid == 0)
-	// 	{
-	// 		dup2(pipe_arr[i - 1][0], STDIN_FILENO);
-	// 		dup2(pipe_arr[i][1], STDOUT_FILENO);
-	// 		close_pipes(pipe_arr, g_info->nb_pipe);
-	// 		cmd_exec(cmds[i]);
-	// 	}
-	// 	i++;
-	// }
-	pid = fork();
-	if (pid == 0)
-	{
-		printf("%d\n", i);
-		// dup2(pipe_arr[i - 1][0], STDIN_FILENO);
-		// close_pipes(pipe_arr, g_info->nb_pipe);
-		cmd_exec(cmds[i]);
-	}
-	i = 0;
-	while (i < g_info->nb_pipe + 1)
-	{
+	close_pipes(pipe_arr, nb_pipe);
+	i = -1;
+	while (++i < nb_pipe + 1)
 		wait(NULL);
-		i++;
-	}
 	return (0);
 }
+
 int main(int ac, char **av, char **env)
 {
 	t_global_info *g_info;
 	t_command **cmd;
 	int i;
 	i = 0;
-	cmd = malloc(sizeof(t_command *) * 2);
-	while (i < 2)
+	cmd = malloc(sizeof(t_command *) * 3);
+	while (i < 3)
 	{
 		cmd[i] = malloc(sizeof(t_command));
 		i++;
@@ -109,12 +94,12 @@ int main(int ac, char **av, char **env)
 	g_info->environ = dup_env(env);
 	g_info->export_env = dup_env(env);
 	g_info->env_array = convert_env_array(g_info->environ);
-	g_info->nb_pipe = 1;
+	g_info->nb_pipe = 2;
 
 	cmd[0]->g_info = g_info;
 	cmd[0]->cmd = ft_strdup("ls");
 	cmd[0]->command_path = cmd_path(cmd[0]->cmd, cmd[0]->g_info->env_array);
-	cmd[0]->cmd_parameter = ft_split("ls -l", ' ');
+	cmd[0]->cmd_parameter = ft_split("ls -la", ' ');
 	/* redirect */
 	cmd[0]->built_in = false;
 	cmd[0]->redirect_in = false;
@@ -123,9 +108,9 @@ int main(int ac, char **av, char **env)
 	cmd[0]->herdoc = false;
 
 	cmd[1]->g_info = g_info;
-	cmd[1]->cmd = ft_strdup("ls");
+	cmd[1]->cmd = ft_strdup("grep");
 	cmd[1]->command_path = cmd_path(cmd[1]->cmd, cmd[1]->g_info->env_array);
-	cmd[1]->cmd_parameter = ft_split("ls -l", ' ');
+	cmd[1]->cmd_parameter = ft_split("grep Apr", ' ');
 	/* redirect */
 	cmd[1]->built_in = false;
 	cmd[1]->redirect_in = false;
@@ -133,16 +118,16 @@ int main(int ac, char **av, char **env)
 	cmd[1]->redirect_out = false;
 	cmd[1]->herdoc = false;
 
-	// cmd[2]->g_info = g_info;
-	// cmd[2]->cmd = ft_strdup("wc");
-	// cmd[2]->command_path = cmd_path(cmd[2]->cmd, cmd[2]->g_info->env_array);
-	// cmd[2]->cmd_parameter = ft_split("wc -l", ' ');
-	// /* redirect */
-	// cmd[2]->built_in = false;
-	// cmd[2]->redirect_in = false;
-	// cmd[2]->redirect_append = false;
-	// cmd[2]->redirect_out = false;
-	// cmd[2]->herdoc = false;
+	cmd[2]->g_info = g_info;
+	cmd[2]->cmd = ft_strdup("wc");
+	cmd[2]->command_path = cmd_path(cmd[2]->cmd, cmd[2]->g_info->env_array);
+	cmd[2]->cmd_parameter = ft_split("wc -l", ' ');
+	/* redirect */
+	cmd[2]->built_in = false;
+	cmd[2]->redirect_in = false;
+	cmd[2]->redirect_append = false;
+	cmd[2]->redirect_out = false;
+	cmd[2]->herdoc = false;
 
-	pipes(cmd, g_info);
+	pipes(cmd, g_info->nb_pipe);
 }
